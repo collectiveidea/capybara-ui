6bfa8aa7c8068aca00f94c8f8ccaceaa9cee4355
Steve Richert <steve.richert@gmail.com>
2013-12-11 16:46:54 -0500

    bundle gem capybara-ui

---
+++ lib/capybara/ui.rb
@@ -0,0 +1,7 @@
+require "capybara/ui/version"
+
+module Capybara
+  module Ui
+    # Your code goes here...
+  end
+end
---
+++ lib/capybara/ui/version.rb
@@ -0,0 +1,5 @@
+module Capybara
+  module Ui
+    VERSION = "0.0.1"
+  end
+end
