a8e1444d46743244e94d5450fd9c0a94f4f266f3
Steve Richert <steve.richert@gmail.com>
2013-12-22 11:22:21 -0500

    Ensure the rack_test driver reloads only the proper session

--- lib/capybara/ui/drivers/rack_test.rb
+++ lib/capybara/ui/drivers/rack_test.rb
@@ -1,11 +1,12 @@
 require "capybara/rack_test/browser"
 
 Capybara::RackTest::Browser.class_eval do
-  def reset_cache_with_ui!
-    Capybara.current_session.reload_ui
-    reset_cache_without_ui!
+  def reset_cache_with_ui_reload!
+    session = Capybara::UI.find_session { |s| s.driver.browser == self }
+    session.reload_ui if session
+    reset_cache_without_ui_reload!
   end
 
-  alias_method :reset_cache_without_ui!, :reset_cache!
-  alias_method :reset_cache!, :reset_cache_with_ui!
+  alias_method :reset_cache_without_ui_reload!, :reset_cache!
+  alias_method :reset_cache!, :reset_cache_with_ui_reload!
 end
