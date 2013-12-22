a033bed0fddf982e4907f9b289a75c0ec784bc07
Steve Richert <steve.richert@gmail.com>
2013-12-22 11:47:16 -0500

    Start to DRY up UI reload

--- lib/capybara/ui/drivers/rack_test.rb
+++ lib/capybara/ui/drivers/rack_test.rb
@@ -1,12 +1,17 @@
 require "capybara/rack_test/browser"
 
 Capybara::RackTest::Browser.class_eval do
-  def reset_cache_with_ui_reload!
-    session = Capybara::UI.find_session { |s| s.driver.browser == self }
-    session.reload_ui if session
-    reset_cache_without_ui_reload!
+  include Capybara::UI::Reload
+
+  has_capybara_ui_session do |browser, session|
+    session.driver.browser == browser
   end
 
-  alias_method :reset_cache_without_ui_reload!, :reset_cache!
-  alias_method :reset_cache!, :reset_cache_with_ui_reload!
+  def reset_cache_with_capybara_ui_reload!
+    capybara_ui_reload
+    reset_cache_without_capybara_ui_reload!
+  end
+
+  alias_method :reset_cache_without_capybara_ui_reload!, :reset_cache!
+  alias_method :reset_cache!, :reset_cache_with_capybara_ui_reload!
 end
---
+++ lib/capybara/ui/reload.rb
@@ -0,0 +1,36 @@
+module Capybara
+  class UI
+    module Reload
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+
+      module ClassMethods
+        def has_capybara_ui_session(&block)
+          @capybara_ui_session_finder = block
+        end
+
+        def capybara_ui_session_finder
+          @capybara_ui_session_finder ||= proc { |instance, session| }
+        end
+      end
+
+      def capybara_ui_reload
+        capybara_ui_session.reload_ui if capybara_ui_session
+      end
+
+      def capybara_ui_session
+        return @capybara_ui_session if defined? @capybara_ui_session
+        @capybara_ui_session = find_capybara_ui_session
+      end
+
+      def find_capybara_ui_session
+        Capybara::UI.find_session(&capybara_ui_session_finder)
+      end
+
+      def capybara_ui_session_finder
+        self.class.capybara_ui_session_finder.curry[self]
+      end
+    end
+  end
+end
