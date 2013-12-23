ed38f7bfaeb579d5e259a9682aeda2b712a446ed
Steve Richert <steve.richert@gmail.com>
2013-12-23 11:41:03 -0500

    Reload the UI when a session is reset

--- lib/capybara/ui/session.rb
+++ lib/capybara/ui/session.rb
@@ -1,12 +1,24 @@
 module Capybara
   class UI
     module Session
+      def self.included(base)
+        base.class_eval do
+          alias_method :reset_without_capybara_ui_reload!, :reset!
+          alias_method :reset!, :reset_with_capybara_ui_reload!
+        end
+      end
+
       def ui
         @ui ||= Capybara::UI.for_page(self)
       end
 
       def reload_ui
         @ui = nil
+      end
+
+      def reset_with_capybara_ui_reload!
+        reload_ui
+        reset_without_capybara_ui_reload!
       end
     end
   end
